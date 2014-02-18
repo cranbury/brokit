38cc69e664f8adeb078c1db76d2f055c1fb5eb72
Granger <GrangerAbuhoff@gmail.com>
2014-02-18 15:27:38 -0500

    Add status to apartment table

--- apt_broker_app/db/schema.rb
+++ apt_broker_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140218201714) do
+ActiveRecord::Schema.define(version: 20140218202326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140218201714) do
     t.text    "photo_url"
     t.integer "landlord_id"
     t.string  "neighborhood"
+    t.text    "status",       default: "available"
   end
 
   create_table "client_favorites", force: true do |t|
