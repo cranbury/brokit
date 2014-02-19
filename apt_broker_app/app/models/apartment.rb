eff7a15fb89d6a3067578cd5eb029f142ee9e9e5
Granger <GrangerAbuhoff@gmail.com>
2014-02-19 09:38:03 -0500

    add dependent destroy for client_favorites

--- apt_broker_app/app/models/apartment.rb
+++ apt_broker_app/app/models/apartment.rb
@@ -1,6 +1,6 @@
 class Apartment < ActiveRecord::Base
   belongs_to :landlord
-  has_many :client_favorites
+  has_many :client_favorites, dependent: :destroy
   has_many :clients, through: :client_favorites
 
   searchable do
